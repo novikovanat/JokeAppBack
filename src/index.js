2f5dbd308ce342ca20dec37dc4e9d1bfabe6ff91
novikovanat <72397136+novikovanat@users.noreply.github.com>
2025-02-16 14:23:57 +0100

    initial

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { initMongoConnection } from './db/initMongoConnection.js';
+import { setupServer } from './server.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const bootstrap = async () => {
+  await initMongoConnection();
+  setupServer();
+};
+
+bootstrap();
---
+++ src/server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { env } from './utils/env.js';
+
+
+dotenv.config();
+const PORT = Number(env('PORT'));
+
+export const setupServer = () => {
+  const app = express();
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.use(express.json());
+  app.use(cors());
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+
+  
+};
