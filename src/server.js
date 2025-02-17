9deab2e4ba72ef402a8b542dba40204412b4c962
novikovanat <72397136+novikovanat@users.noreply.github.com>
2025-02-17 16:12:17 +0100

    script added

--- src/server.js
+++ src/server.js
@@ -1,34 +1,30 @@
-import express from 'express';
-import pino from 'pino-http';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import  env  from './utils/env.js';
-import  jokesRouter  from './routes/routes.js';
-import { notFoundHandler, ErrorHandler } from './middleware/errorHandlers.js';
-
+import express from "express";
+import pino from "pino-http";
+import cors from "cors";
+import dotenv from "dotenv";
+import env from "./utils/env.js";
+import jokesRouter from "./routes/routes.js";
+import { notFoundHandler, ErrorHandler } from "./middleware/errorHandlers.js";
 
 dotenv.config();
-const PORT = Number(env('PORT'));
+const PORT = Number(env("PORT"));
 
 export const setupServer = () => {
   const app = express();
   app.use(
     pino({
       transport: {
-        target: 'pino-pretty',
+        target: "pino-pretty",
       },
-    }),
+    })
   );
 
   app.use(express.json());
   app.use(cors());
-  app.use('/', jokesRouter);
-  app.use('*', notFoundHandler);
-
-  app.use(ErrorHandler);
+  app.use("/", jokesRouter);
+  app.use("*", notFoundHandler);
+  // app.use(ErrorHandler);
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
-
-  
 };
